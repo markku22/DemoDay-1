cdfd2647690425be92f534a11f02566efeafa82e
Järvelin Markku <s1900552@edu.bc.fi>
2019-10-01 13:49:43 +0300

    Projektin liittäminen GitHubiin

---
+++ js/script.js
@@ -0,0 +1,186 @@
+/* =========================================
+            Preloader
+========================================== */
+$(window).on('load', function () { // makes sure that whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+
+/* =========================================
+            Team
+========================================== */
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            }
+        }
+    });
+});
+
+
+
+/* =========================================
+            Progress Bars
+========================================== */
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+});
+
+
+/* =========================================
+            Responsive Tabs
+========================================== */
+$(function () {
+
+    $("#services-tabs").responsiveTabs({
+        animation: 'slide'
+
+    });
+
+});
+
+
+/* =========================================
+            Clients
+========================================== */
+
+/* tähän clients-osio */
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-reght"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 2
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 3
+            },
+            // breakpoint from 480 up
+            768: {
+                items: 6
+            }
+        }
+    });
+});
+
+
+/* =========================================
+            Google Map
+========================================== */
+
+/* =========================================
+            Navigation
+========================================== */
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-dark1.png");
+
+            // Show back to top button
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-blank.png");
+
+            // Hide back to top button
+            $("#back-to-top").fadeOut();
+        }
+    }
+});
+
+// Smooth Scrolling
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get selection id like #abou, #services, #work, #team, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo");
+
+    });
+});
+
+/* =========================================
+            Mobile Menu
+========================================== */
+
+$(function () {
+
+    // Show mobile nav
+    $("#mobile-nav-opn-btn").click(function() {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function() {
+        $("#mobile-nav").css("height", "0%");
+    });
+    
+});
